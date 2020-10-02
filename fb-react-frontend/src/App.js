0ae218825acef6e4cf5f415c31e1b55ade1c591a
Alkesh Sandal <alkesh18@hotmail.com>
2020-10-02 12:25:58 -0400

    project created and cleaned.

---
+++ fb-react-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      {/* header */}
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
