1154c6a77b6a80009db0d25c7ca336671cc43aab
Alkesh Sandal <alkesh18@hotmail.com>
2020-10-02 15:20:02 -0400

    header and sidebar complete (frontend)

---
+++ fb-react-frontend/src/Components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Sidebar.css'
+import SidebarRow from './SidebarRow'
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags'
+import PeopleIcon from '@material-ui/icons/People'
+import ChatIcon from '@material-ui/icons/Chat'
+import StorefrontIcon from '@material-ui/icons/Storefront'
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
+import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined'
+// import { Collapse } from '@material-ui/core'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <SidebarRow src="" title="Alkesh" />
+            <SidebarRow Icon={LocalHospitalIcon} title='Covid 19 Info Center' />
+            <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
+            <SidebarRow Icon={PeopleIcon} title='Friends' />
+            <SidebarRow Icon={ChatIcon} title='Messenger' />
+            <SidebarRow Icon={StorefrontIcon} title='Marketplace' />
+            <SidebarRow Icon={VideoLibraryIcon} title='Videos' />
+            <SidebarRow Icon={ExpandMoreOutlined} title='More' />
+        </div>
+    )
+}
+
+export default Sidebar
